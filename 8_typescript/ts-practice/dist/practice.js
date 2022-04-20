f994926fcebfc521a488e3bec6690ad123be94f3
franco <kws60000@naver.com>
2022-04-20 23:27:34 +0900

    클래스에서 interface 를 implement 하기

---
+++ 8_typescript/ts-practice/dist/practice.js
@@ -0,0 +1,28 @@
+"use strict";
+function sumArray(numbers) {
+    return numbers.reduce(function (acc, cur) { return acc + cur; }, 0);
+}
+var total = sumArray([1, 2, 3, 4, 5]);
+var Circle = /** @class */ (function () {
+    function Circle(radius) {
+        this.radius = radius;
+    }
+    Circle.prototype.getArea = function () {
+        return this.radius * this.radius * Math.PI;
+    };
+    return Circle;
+}());
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}());
+var shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach(function (shape) {
+    console.log(shape.getArea());
+});
