b885350b2bdbc39b754e952e65e4024a6160464c
franco <kws60000@naver.com>
2022-04-21 22:54:14 +0900

    Type, Generics 사용하기

--- 8_typescript/ts-practice/dist/practice.js
+++ 8_typescript/ts-practice/dist/practice.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 function sumArray(numbers) {
     return numbers.reduce(function (acc, cur) { return acc + cur; }, 0);
 }
@@ -26,3 +37,58 @@ var shapes = [new Circle(5), new Rectangle(10, 5)];
 shapes.forEach(function (shape) {
     console.log(shape.getArea());
 });
+var person = {
+    name: '김사랑'
+};
+var expert = {
+    name: '김계발',
+    skills: ['javascript', 'vue']
+};
+var people = [person, expert];
+var color = 'red';
+var colors = ['red', 'orange'];
+// Generics
+function merge(a, b) {
+    return __assign(__assign({}, a), b);
+}
+var merged = merge({ foo: 1 }, { bar: 1 });
+function wrap(param) {
+    return {
+        param: param
+    };
+}
+var wrapped = wrap(10);
+var items = {
+    list: ['a', 'b', 'c']
+};
+// 클래스에서 Generics 사용하기
+var Queue = /** @class */ (function () {
+    function Queue() {
+        this.list = [];
+    }
+    Object.defineProperty(Queue.prototype, "length", {
+        get: function () {
+            return this.list.length;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Queue.prototype.enqueue = function (item) {
+        this.list.push(item);
+    };
+    Queue.prototype.dequeue = function () {
+        return this.list.shift();
+    };
+    return Queue;
+}());
+var queue = new Queue();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
--- 8_typescript/ts-practice/src/practice.ts
+++ 8_typescript/ts-practice/src/practice.ts
@@ -39,25 +39,119 @@ shapes.forEach(shape => {
   console.log(shape.getArea())
 })
 
+
+
 // 일반 객체를 interfaca 로 타입 설정하기
 
-interface Person {
+// interface Person {
+//   name: string
+//   age?: number
+// }
+
+// interface Developer extends Person {
+//   skills: string[]
+// }
+
+// const person: Person = {
+//   name: '김사랑',
+//   age: 20
+// }
+
+// const expert: Developer = {
+//   name: '김개발',
+//   skills: ['javascript', 'react']
+// }
+
+// const people: Person[] = [person, expert]
+
+
+
+// Type Alias 사용하기
+
+type Person = {
   name: string
   age?: number
 }
 
-interface Developer extends Person {
+type Developer = Person & {
   skills: string[]
 }
 
 const person: Person = {
-  name: '김사랑',
-  age: 20
+  name: '김사랑'
 }
 
 const expert: Developer = {
-  name: '김개발',
-  skills: ['javascript', 'react']
+  name: '김계발',
+  skills: ['javascript', 'vue']
 }
 
-const people: Person[] = [person, expert]
+type People = Person[]
+const people: People = [person, expert]
+
+type Color = 'red' | 'orange' | 'yellow'
+const color: Color = 'red'
+const colors: Color[] = ['red', 'orange']
+
+
+
+// Generics
+
+function merge(a: any, b: any): any {
+  return {
+    ...a,
+    ...b
+  }
+}
+
+const merged = merge({ foo: 1 }, { bar: 1})
+
+function wrap<T>(param: T) {
+  return {
+    param
+  }
+}
+
+const wrapped = wrap(10)
+
+// interface Items<T> {
+//   list: T[]
+// }
+
+// const items: Items<string> = {
+//   list: ['a', 'b', 'c']
+// }
+
+type Items<T> = {
+  list: T[]
+}
+
+const items: Items<string> = {
+  list: ['a', 'b', 'c']
+}
+
+// 클래스에서 Generics 사용하기
+class Queue<T> {
+  list: T[] = []
+  get length() {
+    return this.list.length
+  }
+  enqueue(item: T) {
+    this.list.push(item)
+  }
+  dequeue() {
+    return this.list.shift()
+  }
+}
+
+const queue = new Queue<number>()
+queue.enqueue(0)
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+console.log(queue.dequeue())
+console.log(queue.dequeue())
+console.log(queue.dequeue())
+console.log(queue.dequeue())
+console.log(queue.dequeue())
