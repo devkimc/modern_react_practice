520dcfca495a8eee38590a6e319d8d0dceaafedd
franco <kws60000@naver.com>
2022-04-25 20:28:37 +0900

    리덕스 적용하기

--- 6_redux/learn-redux/src/index.js
+++ 6_redux/learn-redux/src/index.js
@@ -4,17 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import rootReducer from './modules'
 
+/* 스토어 만들기 */
 const store = createStore(rootReducer)
-console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
