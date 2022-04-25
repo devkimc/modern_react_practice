5f46dd7351d38715a5aeabd1994f23d814e5ad54
franco <kws60000@naver.com>
2022-04-25 20:23:32 +0900

    todos 모듈, 루트 리듀서 생성

--- 6_redux/learn-redux/src/index.js
+++ 6_redux/learn-redux/src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import rootReducer from './modules'
+
+const store = createStore(rootReducer)
+console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
